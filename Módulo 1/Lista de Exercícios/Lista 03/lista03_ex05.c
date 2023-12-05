7025830d53b75f864631a75023c2a27d894180ad
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 18:01:55 -0300

    Update lista03_ex05.c

--- Módulo 1/Lista de Exercícios/Lista 03/lista03_ex05.c
+++ Módulo 1/Lista de Exercícios/Lista 03/lista03_ex05.c
@@ -1,16 +1,17 @@
-/*QUEST√O 05: FaÁa um programa que exiba na tela os 50 primeiros termos da seguinte sÈrie: 1, -2, 3, -4, 5, -6 ...*/
+/*
+QUEST√ÉO 05: Fa√ßa um programa que exiba na tela os 50 primeiros termos da seguinte s√©rie: 1, -2, 3, -4, 5, -6 ...
+*/
 
 #include <stdio.h>
 
 void main(){
 	
-	//vari·veis
-	
+	//vari√°veis
 	int i, j;
 	
 	//corpo do programa
-	
 	for (i=1, j=-2; i<=50; i+=2, j-=2){
 		printf("%d , %d , " , i , j);
 	}
+	
 }
