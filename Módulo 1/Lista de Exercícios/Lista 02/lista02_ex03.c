d728c92c110b21d7311d99b59b2b8f36790126f2
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 17:43:13 -0300

    Update lista02_ex03.c

--- Módulo 1/Lista de Exercícios/Lista 02/lista02_ex03.c
+++ Módulo 1/Lista de Exercícios/Lista 02/lista02_ex03.c
@@ -1,27 +1,26 @@
-/*QUESTAO 03: As vendas parceladas se tornaram uma �tima op��o para os lojistas que,
-a cada dia, criam novas promo��es para tentar conquistar novos clientes. 
+/*
+QUESTAO 03: As vendas parceladas se tornaram uma otima opcao para os lojistas que, a cada dia, criam novas promocoes para tentar conquistar novos clientes. 
 
-Fa�a um algoritmo que permita ao lojista informar o pre�o do produto e receber as seguintes 
-informa��es:
-	a) O valor com 10% de desconto para pagamento � vista;
-	b) O valor da presta��o para parcelamento sem juros, em 5x;
-	c) O valor da presta��o para parcelamento com juros, em 10x, com 20% de acr�scimo
-	no valor do produto.
+Faca um algoritmo que permita ao lojista informar o preco do produto e receber as seguintes informacoes:
+
+	a) O valor com 10% de desconto para pagamento a vista;
+	b) O valor da prestacao para parcelamento sem juros, em 5x;
+	c) O valor da prestacao para parcelamento com juros, em 10x, com 20% de acrescimo no valor do produto.
 */
 
-//Importa��o de bibliotecas
+//Importacao de bibliotecas
 #include <stdio.h>
 
 void main ()
 {
-	//declara��o de vari�veis
+	//declaracao de variaveis
 	float valor, aVista, pSemJuros, pComJuros;
 	
 	//leitura do valor do produto
 	printf ("Entre com o valor do produto: ");
 	scanf ("%f", &valor);
 	
-	//calculando o valor de pagamento � vista
+	//calculando o valor de pagamento a vista
 	//aVista = valor - (0.1*valor);
 	aVista = valor*0.9;
 	
