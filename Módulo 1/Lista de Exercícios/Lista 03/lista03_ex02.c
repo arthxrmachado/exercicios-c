bd606ea0e6bb67a3c29751ff97d97edbb7b6e3a9
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 18:00:35 -0300

    Update lista03_ex02.c

--- Módulo 1/Lista de Exercícios/Lista 03/lista03_ex02.c
+++ Módulo 1/Lista de Exercícios/Lista 03/lista03_ex02.c
@@ -1,22 +1,16 @@
-/*
-	FAC - Turma B (27/08/2020)
-	
-	Estruturas de repetiÁ„o
-	
-	QUEST√O 02: FaÁa um programa que exiba todos os elementos da seguinte 
-	sÈrie, assim como a soma destes elementos:
+/*	
+QUEST√ÉO 02: Fa√ßa um programa que exiba todos os elementos da seguinte s√©rie, assim como a soma destes elementos:
 
-	1, 50, 2, 49, 3, 48, 4, 47, 5, 46, ..., 49, 2, 50, 1
-
+1, 50, 2, 49, 3, 48, 4, 47, 5, 46, ..., 49, 2, 50, 1
 */
 
-//importaÁ„o de bibliotecas
+//importa√ß√£o de bibliotecas
 #include <stdio.h>
 
 //main
 void main ()
 {
-	//declaraÁ„o de vari·veis
+	//declara√ß√£o de vari√°veis
 	int i, j, s = 0;		
 	
 	for (i=1,j=50;(i<=50)/*&&(j>=1)*/;i++,j--)   		
