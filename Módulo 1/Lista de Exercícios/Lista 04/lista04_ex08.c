46797981bd383665b48544086182c0fcf03f44d8
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 18:11:40 -0300

    Update lista04_ex08.c

--- Módulo 1/Lista de Exercícios/Lista 04/lista04_ex08.c
+++ Módulo 1/Lista de Exercícios/Lista 04/lista04_ex08.c
@@ -1,28 +1,24 @@
 /*
-   FAC - Turma B
-   Data: 17/09/2020
-   
-   QUEST√O 08:
-   Fazer um programa que auxilie o org„o regulador no c·lculo do total de recursos 
-   arrecadados com a aplicaÁ„o de multas de tr‚nsito. 
-   O programa deve ler as seguintes informaÁıes para cada motorista: 
-   	- O n˙mero da carteira de motorista;
- 	- N˙mero de multas;
+QUEST√ÉO 08: Fazer um programa que auxilie o org√£o regulador no c√°lculo do total de recursos arrecadados com a aplica√ß√£o de multas de tr√¢nsito. 
+
+O programa deve ler as seguintes informa√ß√µes para cada motorista: 
+
+   	- O n√∫mero da carteira de motorista;
+ 	- N√∫mero de multas;
  	- Valor de cada uma das multas. 
 
-   Deve ser exibido o valor da dÌvida de cada motorista e ao final da leitura o total de 
-   recursos arrecadados (somatÛrio de todas as multas). 
-   O programa tambÈm dever· apresentar o n˙mero da carteira do motorista que obteve o 
-   maior n˙mero de multas. 
+Deve ser exibido o valor da d√≠vida de cada motorista e ao final da leitura o total de recursos arrecadados (somat√≥rio de todas as multas). 
+
+O programa tamb√©m dever√° apresentar o n√∫mero da carteira do motorista que obteve o maior n√∫mero de multas. 
 */
 
-//importaÁ„o de bibliotecas
+//importa√ß√£o de bibliotecas
 #include <stdio.h>
 
 //main
 void main ()
 {
-	//declaraÁ„o de vari·veis
+	//declara√ß√£o de vari√°veis
 	int i, cnh, nMultas, maior = -1, maiorCNH;
 	float valor, soma, total = 0;
 	char opcao;
@@ -48,20 +44,20 @@ void main ()
 			soma += valor;
 		}
 		
-		//exibindo a dÌvida do motorista
+		//exibindo a d√≠vida do motorista
 		printf ("\tDivida do motorista de cnh %d: R$ %.2f\n", cnh, soma);
 		
 		//calculando o total a ser arrecadado
 		total += soma;		
 		
-		//determinando o motorista que teve o maior n˙mero de multas
+		//determinando o motorista que teve o maior n√∫mero de multas
 		if (nMultas > maior)
 		{
 			maior = nMultas;
 			maiorCNH = cnh;
 		}
 	
-		//verificar se o usu·rio deseja continuar
+		//verificar se o usu√°rio deseja continuar
 		do
 		{
 			printf ("\nDeseja continuar [S/N]? ");
@@ -69,7 +65,7 @@ void main ()
 			scanf ("%c", &opcao);
 			opcao = toupper (opcao);
 			
-			//enviando msg ao usu·rio caso a opÁ„o seja inv·lida
+			//enviando msg ao usu√°rio caso a op√ß√£o seja inv√°lida
 			if ((opcao != 'S') && (opcao != 'N'))
 			{
 				printf ("\tErro: opcao invalida! Tente novamente.\n");
