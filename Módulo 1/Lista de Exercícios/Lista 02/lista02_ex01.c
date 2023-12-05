32337eec4fb1c397ae35e39cad7a3ab14b644e19
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 17:39:46 -0300

    Update lista02_ex01.c

--- Módulo 1/Lista de Exercícios/Lista 02/lista02_ex01.c
+++ Módulo 1/Lista de Exercícios/Lista 02/lista02_ex01.c
@@ -1,25 +1,25 @@
-/*QUESTAO 01: O IMC (Indice de Massa Corporal) e um criterio da Organizacao Mundial de Saude
-para dar uma indicacao sobre a condicao de peso de uma pessoa adulta. A formula e:
+/*QUESTÃO 01: O IMC (Índice de Massa Corporal) é um critério da Organizacao Mundial de Saude para dar uma indicação sobre a condição de peso de uma pessoa 
+adulta.
 
-							IMC = peso / altura^2
+A fórmula é: IMC = peso / altura^2
 
-Elabore um algoritmo que, dados o peso e a altura de um adulto, determine a sua condicao
-de acordo com a tabela abaixo:
+Elabore um algoritmo que, dado o peso e a altura de um adulto, determine a sua condiçãode acordo com a tabela abaixo:
 
-IMC em adultos Condicao
-IMC < 18,5 			Abaixo do peso
+IMC em adultos:
+
+IMC < 18,5 		Abaixo do peso
 18,5 <= IMC < 25,0 	Peso ideal
 25,0 <= IMC < 30,0 	Sobrepeso
 30,0 <= IMC < 35,0 	Obesidade grau I
 35,0 <= IMC < 40,0 	Obesidade grau II
 IMC >= 40,0 		Obesidade grau III*/
 
-//importacao de bibliotecas
+//importação de bibliotecas
 #include <stdio.h>
 
 void main (){
 
-	//declaracao de variaveis
+	//declaração de variáveis
 	float peso, altura, imc;
 	
 	//leitura dos dados de entrada
@@ -29,10 +29,10 @@ void main (){
 	printf ("Altura: ");
 	scanf ("%f", &altura);
 	
-	//calculando o IMC
+	//calculando o imc
 	imc = peso/(altura*altura);
 	
-	//exibindo imc
+	//exibindo o imc
 	printf ("IMC = %.1f\n", imc);
 	
 	//classificando a condicao da pessoa
@@ -42,25 +42,25 @@ void main (){
 
 	else{
 
-		if (/*(imc >= 18.5) &&*/ imc < 25){
+		if (imc < 25){
 			printf ("Peso ideal");
 		}
 
 		else{
 			
-			if (/*(imc >= 25) &&*/ imc < 30){
+			if (imc < 30){
 				printf ("Sobrepeso");
 			}
 
 			else{
 
-				if (/*(imc >= 30) && */imc < 35){
+				if (imc < 35){
 					printf ("Obesidade grau I");
 				}
 				
 				else{
 
-					if (/*(imc >= 35) &&*/imc < 40){
+					if (imc < 40){
 						printf ("Obesidade grau II");
 					}
 
