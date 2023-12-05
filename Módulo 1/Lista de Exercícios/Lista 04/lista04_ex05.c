ec3208f67077f417692fcbe840195c0dfa6e02ca
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 18:09:36 -0300

    Update lista04_ex05.c

--- Módulo 1/Lista de Exercícios/Lista 04/lista04_ex05.c
+++ Módulo 1/Lista de Exercícios/Lista 04/lista04_ex05.c
@@ -1,10 +1,12 @@
-/*QUEST√O 05: Criar um programa que exiba os N primeiros termos da seguinte sÈrie: 1,2,4,8,16,32,...*/
+/*
+QUEST√ÉO 05: Criar um programa que exiba os N primeiros termos da seguinte s√©rie: 1, 2, 4, 8, 16, 32,...
+*/
 
 #include<stdio.h>
 
 void main(){
 	
-	//vari·veis
+	//vari√°veis
 	
 	int i, n, termo=1;
 	
@@ -16,5 +18,5 @@ void main(){
 	for(i=1; i<=n; i++){
 			printf("%d " , termo);
 			termo=termo*2;
-		}
 	}
+}
