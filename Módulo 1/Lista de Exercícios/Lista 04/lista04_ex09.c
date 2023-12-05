8ac40af57e6649ac2ee36a22745a7a40d948030d
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 18:15:16 -0300

    Update lista04_ex09.c

--- Módulo 1/Lista de Exercícios/Lista 04/lista04_ex09.c
+++ Módulo 1/Lista de Exercícios/Lista 04/lista04_ex09.c
@@ -1,23 +1,14 @@
 /*
-   FAC - Turma B
-   Data: 17/09/2020
-   
-   QUEST√O 09:
-   Escrever um programa que encontre o quinto n˙mero maior que 1000, cuja divis„o por 11 
-   tenha resto 5.
-   
-   
-   1001  1002  1003  1004  1005  1006 ...
-   							1∫
+QUEST√ÉO 09: Escrever um programa que encontre o quinto n√∫mero maior que 1000, cuja divis√£o por 11 tenha resto 5.
 */
 
-//importaÁ„o de bibliotecas
+//importa√ß√£o de bibliotecas
 #include <stdio.h>
 
 //main
 void main ()
 {
-	//declaraÁ„o de vari·veis
+	//declara√ß√£o de vari√°veis
 	int numero = 1000, cont = 0;
 	
 	do
