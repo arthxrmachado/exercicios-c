6f4fde9fb245689f74ffba8d0489c19ffee804f3
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-05 18:20:54 -0300

    Update lista05_ex04.c

--- Módulo 1/Lista de Exercícios/Lista 05/lista05_ex04.c
+++ Módulo 1/Lista de Exercícios/Lista 05/lista05_ex04.c
@@ -1,29 +1,28 @@
-/* Lista V
+/*
+QUEST√ÉO 04: A prefeitura de uma cidade fez uma pesquisa com 300 de seus habitantes, coletando dados sobre o sal√°rio e n√∫mero de filhos. A prefeitura deseja
+saber: 
 
-   QUEST√O 04: A prefeitura de uma cidade fez uma pesquisa com 300 de seus habitantes, 
-   coletando dados sobre o sal·rio e n˙mero de filhos. A prefeitura deseja saber: 
-
-   a) mÈdia do sal·rio da populaÁ„o; 
-   b) mÈdia do n˙mero de filhos; 
-   c) maior sal·rio; 
-   d) percentual de pessoas com sal·rio atÈ R$ 1.000,00. 
+   a) m√©dia do sal√°rio da popula√ß√£o; 
+   b) m√©dia do n√∫mero de filhos; 
+   c) maior sal√°rio; 
+   d) percentual de pessoas com sal√°rio at√© R$ 1.000,00. 
 */
 
-//importaÁ„o de bibliotecas
+//importa√ß√£o de bibliotecas
 #include <stdio.h>
 
-//declaraÁ„o de contantes
+//declara√ß√£o de contantes
 #define QUANT 5
 
 //main
 void main ()
 {
-	//DeclaraÁ„o de vari·veis
+	//Declara√ß√£o de vari√°veis
 	int i, nFilhos, somaFilhos=0, cont1000=0;
 	float salario, somaSalario=0, mediaSalario, mediaFilhos,
 	      maiorSalario=0, perc1000;
 	
-	//repetiÁ„o para a leitura dos dados dos QUANT habitantes
+	//repeti√ß√£o para a leitura dos dados dos QUANT habitantes
 	for (i=1;i<=QUANT;i++)
 	{
 		printf ("Salario: ");
@@ -32,31 +31,31 @@ void main ()
 		printf ("Numero de filhos: ");
 		scanf ("%d", &nFilhos);
 		
-		//calculando a soma dos sal·rios para, ao final, calcular a mÈdia		
+		//calculando a soma dos sal√°rios para, ao final, calcular a m√©dia		
 		somaSalario += salario;
 		
-		//calculando o total de filhos para, ao final, calcular a mÈdia		
+		//calculando o total de filhos para, ao final, calcular a m√©dia		
 		somaFilhos += nFilhos;
 
-		//determinando o maior sal·rio
+		//determinando o maior sal√°rio
 		if (salario > maiorSalario)
 		{
 			maiorSalario = salario;
 		}
 		
-		//verificando quantas pessoas possuem sal·rio atÈ R$ 1.000,00
+		//verificando quantas pessoas possuem sal√°rio at√© R$ 1.000,00
 		if (salario <= 1000)
 		{
 			cont1000++;
 		}
 	}
 	
-	//calculando a mÈdia salarial e a mÈdia do n˙mero de filhos
+	//calculando a m√©dia salarial e a m√©dia do n√∫mero de filhos
 	mediaSalario = somaSalario/QUANT;
-	mediaFilhos = (float)somaFilhos/QUANT;	//aqui foi preciso fazer o casting, pois os dois operandos s„o inteiros
+	mediaFilhos = (float)somaFilhos/QUANT;	//aqui foi preciso fazer o casting, pois os dois operandos s√£o inteiros
 	
-	//calculando o percentual de pessoas com sal·rio atÈ R$ 1.000,00
-	perc1000 = ((float)cont1000/QUANT)*100; //aqui foi preciso fazer o casting, pois os dois operandos s„o inteiros
+	//calculando o percentual de pessoas com sal√°rio at√© R$ 1.000,00
+	perc1000 = ((float)cont1000/QUANT)*100; //aqui foi preciso fazer o casting, pois os dois operandos s√£o inteiros
 	
 	//exibindo os resultados
 	printf ("\n\nMedia salarial: R$ %.2f\n", mediaSalario);
