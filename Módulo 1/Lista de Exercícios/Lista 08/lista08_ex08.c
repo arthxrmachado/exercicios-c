c37e90d41a05085456f527ea973c0e3b56a55550
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-08 19:42:12 -0300

    Update lista08_ex08.c

--- Módulo 1/Lista de Exercícios/Lista 08/lista08_ex08.c
+++ Módulo 1/Lista de Exercícios/Lista 08/lista08_ex08.c
@@ -1,47 +1,39 @@
 /*
-   FAC - Turma B - 2020/1
-   Professor Leonardo Vianna
-   
-   Lista de Exercícios VIII (Funções):
-   
-   QUESTÃO 08:
-   Desenvolver uma função que exiba todos os números primos 
-   inferiores a determinado valor inteiro N.
-
+QUESTÃƒO 08: Desenvolver uma funÃ§Ã£o que exiba todos os nÃºmeros primos inferiores a determinado valor inteiro N.
 */
 
-//importação de bibliotecas
+//importaÃ§Ã£o de bibliotecas
 #include <stdio.h>
 
-//protótipos das funções
+//protÃ³tipos das funÃ§Ãµes
 void exibirPrimos (int n);
 
 //main
 void main ()
 {	
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int numero;
 	
 	//lendo o valor de entrada
 	printf ("Entre com um numero: ");
 	scanf ("%d", &numero);
 	
-	//chamando a função
+	//chamando a funÃ§Ã£o
 	exibirPrimos (numero);
 }
 
-//implementação das funções
+//implementaÃ§Ã£o das funÃ§Ãµes
 void exibirPrimos (int n)
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int i, j, cont;
 	
-	//varrendo os números de 2 a té n (excluindo o 1, pois não é primo)
+	//varrendo os nÃºmeros de 2 a tÃ© n (excluindo o 1, pois nÃ£o Ã© primo)
 	for (i=2;i<=n;i++)
 	{
 		cont = 0;
 		
-		//contando o número de divisores de 'i'
+		//contando o nÃºmero de divisores de 'i'
 		for (j=1;j<=i;j++)
 		{
 			if (i%j==0)
@@ -50,7 +42,7 @@ void exibirPrimos (int n)
 			}
 		}
 		
-		//verificando se é primo
+		//verificando se Ã© primo
 		if (cont == 2)
 		{
 			printf ("%d ", i);
