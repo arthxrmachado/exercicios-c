a44c8ed388c03082cb1e5c44213c694a37f9633c
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-08 19:04:27 -0300

    Update lista06_ex04.c

--- Módulo 1/Lista de Exercícios/Lista 06/lista06_ex04.c
+++ Módulo 1/Lista de Exercícios/Lista 06/lista06_ex04.c
@@ -1,29 +1,23 @@
 /*
-   FAC - Turma B
-   
-   Lista de Exercícios VI
-   
-   QUESTÃO 04: Faça um programa que leia um número inteiro N 
-   e informe se o mesmo é ou não primo.
-   
-   Nota: um número é dito primo quando for divisível apenas 
-   por 1 e por ele mesmo.
+QUESTÃƒO 04: FaÃ§a um programa que leia um nÃºmero inteiro N e informe se o mesmo Ã© ou nÃ£o primo.
+
+Nota: um nÃºmero Ã© dito primo quando for divisÃ­vel apenas por 1 e por ele mesmo.
 */
 
-//importação de bibliotecas
+//importaÃ§Ã£o de bibliotecas
 #include <stdio.h>
 
 //main
 void main ()
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int n, i, cont=0;
 	
 	//lendo o valor de n
 	printf ("Entre com um numero: ");
 	scanf ("%d", &n);
 	
-	//contando o número de divisores de n
+	//contando o nÃºmero de divisores de n
 	for (i=1;i<=n;i++)
 	{
 		if (n%i==0)
@@ -32,7 +26,7 @@ void main ()
 		}
 	}
 	
-	//verificando se n é primo
+	//verificando se n Ã© primo
 	if (cont == 2)
 	{
 		printf ("%d eh primo", n);
