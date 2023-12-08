c94b93fa1bb6d42258217d39acbdd94264aebb68
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-08 19:52:51 -0300

    Update lista10_ex08.c

--- Módulo 1/Lista de Exercícios/Lista 10/lista10_ex08.c
+++ Módulo 1/Lista de Exercícios/Lista 10/lista10_ex08.c
@@ -1,33 +1,24 @@
 /*
-   FAC - Turma B
-   Professor Leonardo Vianna
-   Data: 19/11/2020
-   
-   Lista de Exercícios X
-   
-   QUESTÃO 08:
-   Dados um vetor de reais (cujos elementos estão ordenados 
-   crescentemente) e um número x, retornar a posição da primeira 
-   ocorrência de x (caso encontre-se no vetor) ou a posição na 
-   qual deveria estar (caso contrário).
-   
+QUESTÃƒO 08: Dados um vetor de reais (cujos elementos estÃ£o ordenados crescentemente) e um nÃºmero x, retornar a posiÃ§Ã£o da primeira ocorrÃªncia de x 
+(caso encontre-se no vetor) ou a posiÃ§Ã£o na qual deveria estar (caso contrÃ¡rio).
+  
    Exemplo:
    
    vetor:  1  4  8  8  9  12 12 12 15 20		
            0  1  2  3  4  5  6  7  8  9  	quantidade: 10
            
-   buscar o nº 15: retornar 8    
-   buscar o nº 12: retornar 5
-   buscar o nº 10: retornar 5   
+   buscar o nÂº 15: retornar 8    
+   buscar o nÂº 12: retornar 5
+   buscar o nÂº 10: retornar 5   
 */
 
-//importação de bibliotecas
+//importaÃ§Ã£o de bibliotecas
 #include <stdio.h>
 
-//declaração de constantes
+//declaraÃ§Ã£o de constantes
 #define CAP 20
 
-//protótipos das funções
+//protÃ³tipos das funÃ§Ãµes
 void preencherVetor (float v[], int tamanho);
 void exibirVetor (float v[], int tamanho);
 int buscar (float v[], int quantidade, float numero);
@@ -35,7 +26,7 @@ int buscar (float v[], int quantidade, float numero);
 //main
 void main()
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	float vetor[CAP];
 	float num;
 	int posicao;
@@ -50,21 +41,21 @@ void main()
     printf ("Entre com o valor a ser buscado: ");
     scanf ("%f", &num);
     
-    //chamando a função
+    //chamando a funÃ§Ã£o
 	posicao = buscar (vetor, CAP, num);
     
 	printf ("\n\nPosicao: %d\n", posicao);
 }
 
-//implementação das funções
+//implementaÃ§Ã£o das funÃ§Ãµes
 
 //preenchendo o vetor com valores gerados randomicamente
 void preencherVetor (float v[], int tamanho)
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int i;
 	
-	//garantindo que a sequência mude a cada alteração
+	//garantindo que a sequÃªncia mude a cada alteraÃ§Ã£o
 	srand (time(NULL));  //semente - seed
 	
 	//inserindo o primeiro elemento do vetor
@@ -80,7 +71,7 @@ void preencherVetor (float v[], int tamanho)
 //exibe na tela os elementos de um vetor com 'tamanho' elementos
 void exibirVetor (float v[], int tamanho)
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int i;	
 	
 	printf ("\n\nVetor: ");
@@ -95,7 +86,7 @@ void exibirVetor (float v[], int tamanho)
 
 int buscar (float v[], int quantidade, float numero)
 {
-	//declaração de variáveis;
+	//declaraÃ§Ã£o de variÃ¡veis;
 	int i;
 	
 	//varrendo o vetor em busca do elemento
@@ -120,6 +111,6 @@ int buscar (float v[], int quantidade, float numero)
 		}
 	}
 	
-	//retornando a posição, caso o número seja maior do que todos no vetor
+	//retornando a posiÃ§Ã£o, caso o nÃºmero seja maior do que todos no vetor
 	return quantidade; //ou return i;
 }
