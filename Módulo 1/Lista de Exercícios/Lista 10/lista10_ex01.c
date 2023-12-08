34b44fda355635998c8c2ae744c06abcd1452b21
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-08 19:47:56 -0300

    Update and rename lista10_ex01-v2.c to lista10_ex01.c

--- Módulo 1/Lista de Exercícios/Lista 10/lista10_ex01.c
+++ Módulo 1/Lista de Exercícios/Lista 10/lista10_ex01.c
@@ -1,48 +1,43 @@
 /*
-	FAC		- 	   Turma B
-	Lista de Exercícios X
-	
-	QUESTÃO 01:
-	Desenvolver uma função que determine o número de ocorrências 
-	de um número inteiro x em um vetor A.
+QUESTÃƒO 01: Desenvolver uma funÃ§Ã£o que determine o nÃºmero de ocorrÃªncias de um nÃºmero inteiro x em um vetor A.
 */
 
-//importação de bibliotecas
+//importaÃ§Ã£o de bibliotecas
 #include <stdio.h>
 
-//declaração de constantes
+//declaraÃ§Ã£o de constantes
 #define TAM 10
 
-//protótipos das funções
+//protÃ³tipos das funÃ§Ãµes
 int numOcorrencias (int x, int v[TAM]);
 
 //main
 void main ()
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int quant, numero, i, vetor[TAM] = {1,7,9,5,7,1,2,7,1,7};
 		
-	//lendo o número a ser buscado
+	//lendo o nÃºmero a ser buscado
 	printf ("\n\nEntre com o numero a ser buscado no vetor: ");
 	scanf ("%d", &numero);
 	
-	//chamando a função
+	//chamando a funÃ§Ã£o
 	quant = numOcorrencias (numero, vetor);
 	
 	//exibindo o resultado
 	printf ("\n\nForam encontradas %d ocorrencias do numero %d no vetor.", quant, numero);
 }
 
-//implementação das funções
+//implementaÃ§Ã£o das funÃ§Ãµes
 int numOcorrencias (int x, int v[TAM])
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int i, cont = 0;
 	
 	//varrendo o vetor
 	for (i=0;i<TAM;i++)
 	{
-		//verificando se o elemento do vetor é o 'x'
+		//verificando se o elemento do vetor Ã© o 'x'
 		if (v[i] == x)
 		{
 			cont++;
