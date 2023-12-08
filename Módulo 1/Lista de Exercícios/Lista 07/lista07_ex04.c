9c0a7fe75e3cddfc5cff2c18dead6cc70ac87442
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-08 19:07:50 -0300

    Update lista07_ex04.c

--- Módulo 1/Lista de Exercícios/Lista 07/lista07_ex04.c
+++ Módulo 1/Lista de Exercícios/Lista 07/lista07_ex04.c
@@ -1,29 +1,23 @@
 /*
-	FAC - 2020/1
-	Lista de Exercícios VII
-	
-	Questão 04:
-	Na matemática, dois números são chamados de primos entre si quando o seu único divisor 
-	em comum é a unidade (ou seja, o número 1). 
-	Considerando este conceito, implementar um programa que, dados dois números inteiros, 
-	determine se são ou não primos entre si.
+QuestÃ£o 04: Na matemÃ¡tica, dois nÃºmeros sÃ£o chamados de primos entre si quando o seu Ãºnico divisor em comum Ã© a unidade (ou seja, o nÃºmero 1). Considerando 
+este conceito, implementar um programa que, dados dois nÃºmeros inteiros, determine se sÃ£o ou nÃ£o primos entre si.
 */
 
-//importação de bibliotecas
+//importaÃ§Ã£o de bibliotecas
 #include <stdio.h>
 
 //main
 void main ()
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int num1, num2, i, primos, menor;
 	
-	//leitura dos dois números
+	//leitura dos dois nÃºmeros
 	printf ("Entre com os dois numeros: ");
 	scanf ("%d %d", &num1, &num2);
 
 	//determinando o menor entre os valores
-	//afinal, basta verificar se existe algum divisor comum entre os dois números no 
+	//afinal, basta verificar se existe algum divisor comum entre os dois nÃºmeros no 
 	//intervalo de 2 ao menor dos valores
 	if (num1 < num2)
 	{
@@ -36,12 +30,12 @@ void main ()
 	
 	//ou apenas: menor = (num1<num2)?num1:num2;
 	
-	//inicialmente, considerando que são primos entre si. Se algum divisor comum for
-	//encontrado, essa variável reberá 0
+	//inicialmente, considerando que sÃ£o primos entre si. Se algum divisor comum for
+	//encontrado, essa variÃ¡vel reberÃ¡ 0
 	primos = 1;
 	
-	//verificando se há algum divisor comum entre os dois elementos
-	for (i=2;(i<=menor)&&(primos==1);i++)  //se a variável 'primos' mudar para 0, já sabemos que não são primos entre si. Portanto, pode parar a busca.
+	//verificando se hÃ¡ algum divisor comum entre os dois elementos
+	for (i=2;(i<=menor)&&(primos==1);i++)  //se a variÃ¡vel 'primos' mudar para 0, jÃ¡ sabemos que nÃ£o sÃ£o primos entre si. Portanto, pode parar a busca.
 	{
 		if ((num1%i==0) && (num2%i==0))
 		{
@@ -49,7 +43,7 @@ void main ()
 		}
 	}
 	
-	//verificando se é ou não primo
+	//verificando se Ã© ou nÃ£o primo
 	if (primos == 1)
 	{
 		printf ("Os numeros sao primos entre si.\n");
