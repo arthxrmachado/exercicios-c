aa57dc6bdd6e7d9b9005ee77a799cdf3af8bd296
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-08 19:01:37 -0300

    Update lista06_ex03-v2.c

--- Módulo 1/Lista de Exercícios/Lista 06/lista06_ex03-v2.c
+++ Módulo 1/Lista de Exercícios/Lista 06/lista06_ex03-v2.c
@@ -1,28 +1,14 @@
 /*
-   FAC - Turma B
-   
-   Lista de Exercícios VI
-   
-   QUESTÃO 03: Faça um programa que exiba na tela as tabuadas 
-   dos números de 1 a 9, como descrito a seguir:
-
-
-	1 × 0 = 0	2 × 0 = 0	...	9 × 0 = 0	
-	1 × 1 = 1	2 × 1 = 2	...	9 × 1 = 9	
-	1 × 2 = 2	2 × 2 = 4	...	9 × 2 = 18
-		.		.		.
-		.		.		.
-		.		.		.
-	1 × 9 = 9	2 × 9 = 18	...	9 × 9 = 81
+QUESTÃƒO 03: FaÃ§a um programa que exiba na tela as tabuadas dos nÃºmeros de 1 a 9.
 */
 
-//importação de bibliotecas
+//importaÃ§Ã£o de bibliotecas
 #include <stdio.h>
 
 //main
 void main ()
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int i, j, r;
 	
 	//exibindo as tabuadas de 1 a 9
