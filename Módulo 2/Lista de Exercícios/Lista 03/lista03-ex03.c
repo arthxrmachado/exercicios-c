7efdc90021bd2ccafc74141f0b660271b11b5c2d
Arthur Machado <82006333+arthxrmachado@users.noreply.github.com>
2023-12-12 19:32:08 -0300

    Update lista03-ex03.c

--- Módulo 2/Lista de Exercícios/Lista 03/lista03-ex03.c
+++ Módulo 2/Lista de Exercícios/Lista 03/lista03-ex03.c
@@ -1,38 +1,26 @@
-/********************************************************************************************
+// QUESTÃƒO 03: Fazer uma funÃ§Ã£o recursiva que, dado um nÃºmero inteiro N, exiba o mesmo na base 2 (binÃ¡ria).
 
-	FPR - Manhã
-	Professor Leonardo Vianna
-	2021/1
-	Data: 07/04/2021
-	
-	Lista de Exercícios V - Recursividade
-	
-	Exercício 3: Fazer uma função recursiva que, dado um número 
-	inteiro N, exiba o mesmo na base 2 (binária)..
-		
-*******************************************************************************************/
-
-//importação de bibliotecas
+//importaÃ§Ã£o de bibliotecas
 #include <stdio.h>
 
-//protótipos das funções
+//protÃ³tipos das funÃ§Ãµes
 void binario (int n);
 
-//função main
+//funÃ§Ã£o main
 void main ()
 {
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int num;
 	
 	//leitura dos dados
 	printf ("Entre com o valor a ser transformado para a base 2: ");
 	scanf ("%d", &num);
 	
-	//chamando a função	
+	//chamando a funÃ§Ã£o	
   	binario (num);
 }
 
-//implementação das funções
+//implementaÃ§Ã£o das funÃ§Ãµes
 void binario (int n)
 {
   if(n >= 1)
